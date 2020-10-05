17f82f37172429f887d7561bb8a03d6578dfa8ee
Ellie Maidment <ellie947@hotmail.com>
2020-10-05 10:03:15 +0100

    implement Textfield in URLInput

--- src/components/URLInput.js
+++ src/components/URLInput.js
@@ -3,18 +3,10 @@ import { TextField, Button, Grid, Container } from '@material-ui/core'
 
 function URLInput() {
   return (
-    <Container maxWidth="md">
-    <Grid container spacing={3}>
-            <Grid item xs={12} md={6}>
-                <TextField id="outlined-basic" label="Input URL..." variant="outlined" fullWidth />
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Button variant="contained" color="primary">Analyze</Button>
-            </Grid>
-    </Grid>
-    </Container>
-   
-  );
+  <form>
+    <TextField variant="outlined"></TextField>
+  </form>  
+  )
 }
 
 export default URLInput;
