3060c4fedeb2a0ed6c08291160bb276f3e2951a2
Ellie Maidment <ellie947@hotmail.com>
2020-10-05 10:38:09 +0100

    implement Grid compoonent

--- src/components/URLInput.js
+++ src/components/URLInput.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import Textfield  from '@material-ui/core/Textfield';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Textfield from "@material-ui/core/Textfield";
+import Button from "@material-ui/core/Button";
 /*error toggle needs implementing*/
-
 
 function URLInput() {
   return (
-    <div style={{border: "1px solid red"}}>
-  <form>
-    <Textfield required variant="outlined" label="Input URL"></Textfield>
-    <Button size="large" variant="contained" color="primary" disableElevation>Analyse</Button>
-  </form>  
-  </div>
-  )
+    <div>
+      <Grid style={{ border: "1px solid red" }}>
+        <form>
+          <Textfield required variant="outlined" label="Input URL"></Textfield>
+          <Button
+            size="large"
+            variant="contained"
+            color="primary"
+            disableElevation
+          >
+            Analyse
+          </Button>
+        </form>
+      </Grid>
+    </div>
+  );
 }
 
 export default URLInput;
