3105ac4cb67162abb779cb5e5150081f13030017
Ellie Maidment <ellie947@hotmail.com>
2020-10-05 13:37:37 +0100

    implement array to store URL for now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import URLInput from './components/URLInput'
 
 function App() {
 
+  //How to store the data? Redux? Rest? Jamie help?
+const inputData = [];
+//just putting data in an array for now. 
+
 const getURL = (text) =>{
-  console.log(text);
+  inputData.push(text);
+  console.log(inputData);
 }
 
 
