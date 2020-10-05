94bf930f8bc24b6074a8669c2f84ed8630b1c0bf
Ellie Maidment <ellie947@hotmail.com>
2020-10-05 10:12:49 +0100

    note: error toggle needs implementing

--- src/components/URLInput.js
+++ src/components/URLInput.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TextField, Button, Grid, Container } from '@material-ui/core'
+/*error toggle needs implementing*/
+
 
 function URLInput() {
   return (
   <form>
     <TextField required variant="outlined" label="Input URL"></TextField>
+    
   </form>  
   )
 }
