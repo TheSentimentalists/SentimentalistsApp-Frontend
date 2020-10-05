4e607ca9a17cd6fda3a752a8b892a72ba6328e81
Ellie Maidment <ellie947@hotmail.com>
2020-10-05 17:32:23 +0100

    implement URLInput.css Graduate Font

--- src/components/URLInput/URLInput.js
+++ src/components/URLInput/URLInput.js
@@ -1,41 +1,66 @@
-import React, { useState } from "react"; 
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Textfield from "@material-ui/core/Textfield";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from '@material-ui/core/styles';
-/*error toggle needs implementing*/
+import { makeStyles } from '@material-ui/core';
+import './URLinput.css';
+/* resolve CSS */
+ /*error toggle needs implementing*/
+
+
+ const useStyles = makeStyles(() => ({
+      inputURLArea : {
+        border: '3px solid red',
+        paddingTop: '25px',
+        paddingBottom: '55px'
+      },
+
+      inputBox : {
+        border: '5px solid #bba5ac',
+        textAlign: 'center',
+        marginBottom: '30px',
+        fontFamily: 'Graduate, sans-serif',
+      },
+
+      inputButton : {
+        background: '#6bb26d',
+        borderRadius: 0,
+        fontFamily: 'Graduate,'
+      },
+    }))
 
 function URLInput(props) {
 const [text, setText] = useState("");
+const classes = useStyles();
 
-const handleTextChange = (event) => {
-  setText(event.target.value);
-}
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
 
+  const handleClick = () => {
+    props.getURL(text);
+  };
 
-const handleClick = () => {
-  props.getURL(text);
-}
-  
-return (
-<div>
-      <Grid container direction="column" style={{ border: "1px solid red" }}>
+  return (
+    <div>
+      <Grid container direction="column" justify="center" align="center"
+      className={classes.inputURLArea}>
         <form>
-          <Grid item xs={12}>
-            <Textfield
+          <Grid item xs={10} sm={8} md={6} lg={4}> 
+            <Textfield className={classes.inputBox}
               fullWidth
               required
-              variant="outlined"
               label="Input URL"
-              value = { text }
-              onChange={ handleTextChange }
+              variant="outlined"
+              value={text}
+              onChange={handleTextChange}
             ></Textfield>
           </Grid>
-          <Button
+          <Button style={{fontFamily: "Graduate"}} className={classes.inputButton}
             size="large"
             variant="contained"
-            color="primary"
-            onClick={ handleClick }
+            
+            onClick={handleClick}
             disableElevation
           >
             Analyse
