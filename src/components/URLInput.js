12638575c60bc99f63634e22ee59413ee3dcf2c9
Ellie Maidment <ellie947@hotmail.com>
2020-10-05 11:08:57 +0100

    import useState

--- src/components/URLInput.js
+++ src/components/URLInput.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react"; 
 import Grid from "@material-ui/core/Grid";
 import Textfield from "@material-ui/core/Textfield";
 import Button from "@material-ui/core/Button";
 /*error toggle needs implementing*/
 
 function URLInput() {
-  return (
 
-const handleTextChange = event => {
-  setText(event.target.value)
-}
+  const [text, setText]
 
-const handleAnalyseClick = () => {
-  addURL(text);
-  setText("");
-}
-
-    <div>
+  const handleTextChange = event => {
+    setText(event.target.value)
+  }
+  
+  const handleAnalyseClick = () => {
+    addURL(text);
+    setText("");
+  }
+  
+  
+return (
+<div>
       <Grid container direction="column" style={{ border: "1px solid red" }}>
         <form>
           <Grid item xs={12}>
