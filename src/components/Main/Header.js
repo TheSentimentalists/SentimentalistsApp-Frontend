8c0f622e2d62db780da5cf281f644619aefa7c69
Ellie Maidment <ellie947@hotmail.com>
2020-10-06 13:20:40 +0100

    implement responsive image

--- src/components/Main/Header.js
+++ src/components/Main/Header.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import Logo from '../../assets/images/Sentimentalists Logo.png';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   mainLogo: {
     display: 'block',
-    margin: 'auto',
+    width: '100%',
+    height: 'auto',
   }
 })
+
 
 function Header() {
 const classes = useStyles();
 
   return (
-    
+    <Grid container justify="center">
+    <Grid item xs={8}>
     <img src={Logo} alt="The Sentimentalists Logo" className={classes.mainLogo}/>
+    </Grid>
+    </Grid>
   );
 }
 
