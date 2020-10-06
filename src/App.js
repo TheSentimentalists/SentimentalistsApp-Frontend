048187e8b581530fccdcc9945173c1bfa53d2e91
Ellie Maidment <ellie947@hotmail.com>
2020-10-06 14:33:15 +0100

    import Graduate and Roboto fonts

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React from 'react';
-import './App.css';
-import { Container, Grid } from '@material-ui/core/';
-import Header from './components/Main/Header';
-import { makeStyles } from '@material-ui/core/styles';
-import URLInput from './components/URLInput';
-
+import React from "react";
+import "./App.css";
+import { Container, Grid } from "@material-ui/core/";
+import Header from "./components/Main/Header";
+import { makeStyles } from "@material-ui/core/styles";
+import URLInput from "./components/URLInput";
 
 const useStyles = makeStyles({
-  containerStyle : {
-    boxShadow: '8px 8px #d9d9d9',
-    background: '#ffffff',
+  containerStyle: {
+    boxShadow: "8px 8px #d9d9d9",
+    background: "#ffffff",
   },
 
   footer: {
-        background: '#6bb26d',
-        padding: '7px',
-  }
-})
+    background: "#6bb26d",
+    padding: "7px",
+  },
+});
 
 function App() {
-const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <div className="App">
       <Container fixed>
-      <div className={classes.containerStyle}>
-      <Header />
+        <div className={classes.containerStyle}>
+          <Header />
 
-      <Grid container className={classes.footer}>
-       <Grid item xs={12}>
-         <h5>The Sentimentalists &copy; 2020</h5>
-       </Grid>
-        </Grid>
-      </div>
-      </Container> 
+          <Grid container className={classes.footer}>
+            <Grid item xs={12}>
+              <h5>The Sentimentalists &copy; 2020</h5>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
     </div>
   );
 }
