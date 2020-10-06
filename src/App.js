fa4de6d3f77b85a28ce6fff3a9709e6d620c2c1b
Ellie Maidment <ellie947@hotmail.com>
2020-10-06 12:38:43 +0100

    New folder, Main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Main/Header'
 import URLInput from './components/URLInput/URLInput'
 
 function App() {
 
-  //How to store the data? Redux? Rest? Jamie help?
+
 const inputData = [];
-//just putting data in an array for now. 
+//just putting data in an array for now. Will be stored as JSON to sent to API
 
 const getURL = (text) =>{
   inputData.push(text);
