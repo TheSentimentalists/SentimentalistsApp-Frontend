2b50b6a299fd4a34f95b037ccce24e00f11df012
Ellie Maidment <ellie947@hotmail.com>
2020-10-07 11:03:12 +0100

    input storage change to object from array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Main/Header'
-import URLInput from './components/URLInput/URLInput'
+import { Container, Grid } from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
+import URLInput from './components/URLInput/URLInput';
+
+//styles
+const useStyles = makeStyles({
+  containerStyle : {
+    boxShadow: '8px 8px #d9d9d9',
+    background: '#ffffff',
+  },
+
+  footer: {
+        background: '#6bb26d',
+        padding: '7px',
+  }
+})
+
+//getUrl
+ const inputData = {
+   url: "",
+ };
+
+ const getURL = (text) => {
+   inputData.url = text;
+   console.log(inputData);
+ }
 
 function App() {
-
-
-const inputData = [];
-//just putting data in an array for now. Will be stored as JSON to sent to API
-
-const getURL = (text) =>{
-  inputData.push(text);
-  console.log(inputData);
-}
-
+const classes = useStyles();
 
   return (
     <div className="App">
-      <Header />
-      <URLInput getURL={ getURL } />
+      <Container fixed>
+      <div className={classes.containerStyle}>
+      
+      <URLInput getURL= { getURL }/>
+
+      <Grid container className={classes.footer}>
+       <Grid item xs={12}>
+         <h5>The Sentimentalists &copy; 2020</h5>
+       </Grid>
+        </Grid>
+      </div>
+      </Container> 
     </div>
   );
 }
--- src/components/Main/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Header() {
-  return (
-    <h1>The Sentimentalists</h1>
-  );
-}
-
-export default Header;
