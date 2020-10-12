be100f1477d338219a6a4f2df39e648dcbf06be8
Ellie Maidment <ellie947@hotmail.com>
2020-10-12 11:40:32 +0100

    implement score /by 100 to provide prop for gauge

--- src/components/Results/displayResults.js
+++ src/components/Results/displayResults.js
@@ -1,18 +1,25 @@
 import React from "react";
-import GaugeChart from 'react-gauge-chart';
+import GaugeChart from "react-gauge-chart";
 
 function DisplayResults(props) {
-const resultsObj = JSON.parse(props.displayResults);
-const credibilityScore = resultsObj[0].score
-// accesses score from credibility analysis, assigns it to a variable to be passed as prop in to the gaugeChart;
+  const resultsObj = JSON.parse(props.displayResults);
+  const credibilityScore = (resultsObj[0].score / 100);
+  // accesses score from credibility analysis, assigns it to a variable, 
+  //the score is divided by 100 to provide the format required forthe percent prop on the gauge.
 
- 
+  console.log(credibilityScore);
 
   return (
     <div>
       <h5>Some results</h5>
       <p>{props.displayResults}</p>
-      <GaugeChart id="main-gauge" hideText={true} textColor="black" nrOfLevels={10}/>
+      <GaugeChart
+        id="main-gauge"
+        percent={credibilityScore}
+        hideText={true}
+        textColor="black"
+        nrOfLevels={10}
+      />
     </div>
   );
 }
