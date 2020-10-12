b01a7aa8fbdfdbe69a58fc744d5bb80675e7551d
Ellie Maidment <ellie947@hotmail.com>
2020-10-12 11:17:41 +0100

    implement destructure JSON to variables

--- src/components/Results/displayResults.js
+++ src/components/Results/displayResults.js
@@ -2,6 +2,12 @@ import React from "react";
 import GaugeChart from 'react-gauge-chart';
 
 function DisplayResults(props) {
+const resultsObj = JSON.parse(props.displayResults);
+const credibilityScore = resultsObj[0].score
+// accesses score from credibility analysis, assigns it to a variable to be passed as prop in to the gaugeChart;
+
+ 
+
   return (
     <div>
       <h5>Some results</h5>
