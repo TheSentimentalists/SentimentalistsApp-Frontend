e28e185c9233f45ccd3dadece9d2a267ccc0fd0a
Jyothsna C Guvvala <jyothsna234@yahoo.com>
2020-10-12 13:46:17 +0100

    With Guage component

--- src/components/Results/displayResults.js
+++ src/components/Results/displayResults.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GaugeChart from 'react-gauge-chart'
 
 function DisplayResults(props) {
   return (
     <div>
       <h5>Some results</h5>
+      <GaugeChart id="gauge-chart1" />
       <p>{props.displayResults}</p>
     </div>
   );
