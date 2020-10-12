bde8f90df16518aed5b770ec07c59c085a5f63df
Ellie Maidment <ellie947@hotmail.com>
2020-10-12 10:43:27 +0100

    import GaugeChart from react-gauge-chart

--- src/components/Results/displayResults.js
+++ src/components/Results/displayResults.js
@@ -1,10 +1,12 @@
 import React from "react";
+import GaugeChart from 'react-gauge-chart';
 
 function DisplayResults(props) {
   return (
     <div>
       <h5>Some results</h5>
       <p>{props.displayResults}</p>
+      <GaugeChart id="main-gauge" hideText={true} textColor="black" nrOfLevels={10}/>
     </div>
   );
 }
