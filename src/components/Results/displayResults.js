1b6f2f6e0a952aa7d20aaf8ac49104b676c25bad
Jyothsna C Guvvala <jyothsna234@yahoo.com>
2020-10-13 16:53:11 +0100

    creating barchart

--- src/components/Results/displayResults.js
+++ src/components/Results/displayResults.js
@@ -1,12 +1,19 @@
 import React from "react";
+import BarChart from "gradient-barchart";
+import 
 
 function DisplayResults(props) {
+  const resultsObj = JSON.parse(props.displayResults);
+  const DATA = [{Type:resultsObj[0].Type,value:resultsObj[0].score, color:"#ee445e"}];
   return (
     <div>
       <h3>Credibility results</h3>
-      
+      <BarChart
+      data={DATA}
+      width={500}
+      height={100}
+      />
     </div>
   );
 }
-
 export default DisplayResults;
