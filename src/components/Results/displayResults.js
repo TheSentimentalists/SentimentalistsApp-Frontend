75ce392ad7393a68d509f384cf7a0964154671fe
Ellie Maidment <ellie947@hotmail.com>
2020-10-21 14:10:41 +0100

    format document

--- src/components/Results/displayResults.js
+++ src/components/Results/displayResults.js
@@ -103,7 +103,6 @@ function DisplayResults(props) {
               <Typography variant="body2" className={classes.resultsText}>
                 {category}
               </Typography>
-  
             </Grid>
           </Grid>
         </Grid>
