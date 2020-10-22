6c3d1b4a3c0bef61b7caec4fa807748410ea0c52
Jyothsna C Guvvala <jyothsna234@yahoo.com>
2020-10-22 08:17:06 +0100

    implemented BiasScoreText styling BProgressBar

--- src/components/Progressbar/BProgressBar.js
+++ src/components/Progressbar/BProgressBar.js
@@ -6,21 +6,23 @@ import Grid from '@material-ui/core/Grid';
 function BProgressBar(){
   return(
     <div>
-    <Grid container 
-    spacing = {1}
-    justify ="center" 
-    alignItems = "center"
-    >
-    <Grid>
-      Bias Score
-    </Grid>
-    <Grid 
-    item xs ={10} sm={10} md={10} lg={10}>
-    <ProgressBar
-    percent={30}
-    unfilledBackground="linear-gradient(to right, #6BB26D, #EE445E)"/>
-    </Grid>
-    </Grid>
+      <Grid container 
+      spacing = {1}
+      justify ="center" 
+      alignItems = "center"
+      >
+      <Grid 
+        item align = "center"
+        item xs ={5} sm={5} md={5} lg={5} >
+        Bias Score
+      </Grid>
+      <Grid 
+        item xs ={10} sm={10} md={10} lg={10}>
+        <ProgressBar
+        percent={30}
+        unfilledBackground="linear-gradient(to right, #6BB26D, #EE445E)"/>
+      </Grid>
+      </Grid>
     </div>
   )
 }
