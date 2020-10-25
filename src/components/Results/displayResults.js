7cbd63508cf890a2558d0e3e129d56638f5034d1
Ellie Maidment <ellie947@hotmail.com>
2020-10-25 11:00:59 +0000

    error fix

--- src/components/Results/displayResults.js
+++ src/components/Results/displayResults.js
@@ -4,23 +4,24 @@ import ArticleSummary from "./ArticleSummary";
 import Credibility from "./Credibility";
 
 function DisplayResults(props) {
-
-  const classes = useStyles();
   const resultsObj = props.displayResults;
-  const analysedURL = resultsObj.url;
-  console.log(resultsObj)
+
+  console.log(resultsObj);
 
   // temporary code to support credibility score while it's here
-  const credibilityScore = resultsObj.results[0].outcome.score / 100
-  const source = resultsObj.results[0].outcome.source
-  const category = resultsObj.results[0].outcome.category
-
+  const credibilityScore = resultsObj.results[0].outcome.score / 100;
+  const source = resultsObj.results[0].outcome.source;
+  const category = resultsObj.results[0].outcome.category;
 
   return (
     <div>
       <ArticleSummary />
 
-      <Credibility score={credibilityScore} category={category} source={source} />
+      <Credibility
+        score={credibilityScore}
+        category={category}
+        source={source}
+      />
     </div>
   );
 }
