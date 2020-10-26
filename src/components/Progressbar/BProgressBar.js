ed00e0a1ace581f4aeb45f5c0a9d5d588706787f
Ellie Maidment <ellie947@hotmail.com>
2020-10-26 15:35:52 +0000

    merge

--- src/components/Progressbar/BProgressBar.js
+++ src/components/Progressbar/BProgressBar.js
@@ -1,50 +1,49 @@
 import React from "react";
 import "react-step-progress-bar/styles.css";
-import { ProgressBar,  } from "react-step-progress-bar";
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { ProgressBar } from "react-step-progress-bar";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  trustScoreHeader : {
+  trustScoreHeader: {
     textAlign: "center",
     paddingBottom: "20px",
   },
-})
+});
 
-function BProgressBar(){
-const classes = useStyles();
+function BProgressBar() {
+  const classes = useStyles();
 
-let percentage = 80
-let fgcolor
- 
-if (percentage < 30) {
-  fgcolor = "linear-gradient(to right, #e60202, #7d1225)";
-} else if(percentage >= 30 && percentage < 70) {
-  fgcolor = "linear-gradient(to right, #ffcf53, #e79b37)";
-} else {
-  fgcolor = "linear-gradient(to right, #396f5c, #5ba45b)";
-}
+  let percentage = 80;
+  let fgcolor;
 
-  return(
+  if (percentage < 30) {
+    fgcolor = "linear-gradient(to right, #e60202, #7d1225)";
+  } else if (percentage >= 30 && percentage < 70) {
+    fgcolor = "linear-gradient(to right, #ffcf53, #e79b37)";
+  } else {
+    fgcolor = "linear-gradient(to right, #396f5c, #5ba45b)";
+  }
+
+  return (
     <div>
-    <Grid container
-    justify="center"
-    >
-    <Grid
-    item xs ={10} sm={10} md={10} lg={10} >
-      <Typography variant="h5" className={classes.trustScoreHeader}>Trust   Score</Typography>
-    </Grid>
-    <Grid item xs ={10} sm={10} md={10} lg={10}>
-    <ProgressBar
-    percent={percentage}
-    unfilledBackground="lightgrey"
-    filledBackground={fgcolor}
-    height={15} />
-    </Grid>
-    </Grid>
-
+      <Grid container justify="center">
+        <Grid item xs={10} sm={10} md={10} lg={10}>
+          <Typography variant="h5" className={classes.trustScoreHeader}>
+            Trust Score
+          </Typography>
+        </Grid>
+        <Grid item xs={10} sm={10} md={10} lg={10}>
+          <ProgressBar
+            percent={percentage}
+            unfilledBackground="lightgrey"
+            filledBackground={fgcolor}
+            height={15}
+          />
+        </Grid>
+      </Grid>
     </div>
-  )
+  );
 }
 export default BProgressBar;
