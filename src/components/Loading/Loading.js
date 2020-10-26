162ea13bb46a56765ccc47aefb055ec44a6a780a
Ellie Maidment <ellie947@hotmail.com>
2020-10-26 15:18:39 +0000

    Merge branch 'Multicolor_progressbar' of

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../App.css";
+
+function Loading() {
+
+  return (
+    <div>
+      <h2>Just a moment...</h2>
+    </div>
+  );
+}
+
+export default Loading;
