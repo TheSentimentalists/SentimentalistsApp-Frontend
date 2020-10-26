53b1e4374030110778c0e7bf4f27da6bb9789aff
Ellie Maidment <ellie947@hotmail.com>
2020-10-26 11:49:57 +0000

    add gauge to Objectivity.js

--- src/components/Results/Objectivity.js
+++ src/components/Results/Objectivity.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import GaugeChart from 'react-gauge-chart';
 
 function Objectivity(props){
+
+const score = props.displayObjectivity;
+
     return(
         <div>
         <p>Objectivity</p>
+        <GaugeChart
+        textColor="#000000"
+        id="objectivity-gauge" 
+        percent={score}
+        hideText={false}
+        nrOfLevels={3}
+        colors={["#ee445e", "#f8ce94", "#6bb26d"]}
+        />
         <p>{props.displayObjectivity}</p>
+        
         </div>
     )
 }
