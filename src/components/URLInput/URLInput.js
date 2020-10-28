f17d856e7db612ce9ba8992496ffedc066720cae
Jamie Johnstone <j@miej.co>
2020-10-28 09:55:07 +0000

    Update URLInput to use correct prod API

--- src/components/URLInput/URLInput.js
+++ src/components/URLInput/URLInput.js
@@ -47,7 +47,7 @@ function URLInput(props) {
     };
 
     fetch(
-      "https://1rrk3o1dq0.execute-api.eu-west-2.amazonaws.com/test/analysis",
+      "https://1rrk3o1dq0.execute-api.eu-west-2.amazonaws.com/prod/analysis",
       requestOptions
     )
       .then(async (response) => {
