d2a8ce8f6d252b7bbf087b92d14f60d1a7b7e9f1
Jyothsna C Guvvala <jyothsna234@yahoo.com>
2020-10-28 19:09:35 +0000

    displayResults props

--- src/components/Results/displayResults.js
+++ src/components/Results/displayResults.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "../../App.css";
-import BProgressBar from "../Progressbar/BProgressBar";
+import BProgressBar from '../Progressbar/BProgressBar';
 import ArticleSummary from "./ArticleSummary";
-import Grid from "@material-ui/core/Grid";
 import Credibility from "./Credibility";
-import Objectivity from "./Objectivity";
+
 
 function DisplayResults(props) {
   const resultsObj = props.displayResults;
@@ -12,32 +11,30 @@ function DisplayResults(props) {
   const analysedURL = resultsObj.url;
   console.log(resultsObj);
 
+  
 const article = resultsObj.article;
 
-const objectivityScore = resultsObj[0].outcome.score;
 
-const BiasScore = resultsObj[0].outcome;
+  // temporary code to support credibility score while it's here
+  const credibilityScore = resultsObj.results[0].outcome.score / 100;
+  const source = resultsObj.results[0].outcome.source;
+  const category = resultsObj.results[0].outcome.category;
+  const BiasScore = resultsObj.results[3].outcome.score;
 
   return (
     <div>
-
-<Grid container>
-
-      <ArticleSummary displayURL={analysedURL} displayArticle={article}/>
-      <BProgressBar Bscore = {BiasScore}/>  
-      <Grid item xs={12} md={4}>
-        <Credibility outcome={resultsObj[0].outcome} />
-      </Grid>
-    
-      <Grid item xs={12} md={4}>
-        <Objectivity displayObjectivity={objectivityScore}/>
-      </Grid>
+    <ArticleSummary displayURL={analysedURL} displayArticle={article}/>
+      <BProgressBar Bscore={BiasScore}/>
+      <Credibility
+        score={credibilityScore}
+        category={category}
+        source={source}
+      />
 
 
-
-</Grid>
     </div>
   );
 }
 
 export default DisplayResults;
+
