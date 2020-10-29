bff6cac69536cb8fe8204f215bdb1d94bfe18820
Jamie Johnstone <j@miej.co>
2020-10-29 01:14:28 +0000

    Fix bad variables

--- src/components/Results/displayResults.js
+++ src/components/Results/displayResults.js
@@ -8,28 +8,30 @@ import Objectivity from "./Objectivity";
 
 function DisplayResults(props) {
   const resultsObj = props.displayResults;
-
   const analysedURL = resultsObj.url;
   console.log(resultsObj);
-
-const article = resultsObj.article;
-
-const objectivityScore = resultsObj.results[2].outcome.score;
+  const article = resultsObj.article;
+  const credibilityObj = resultsObj.results[0].outcome;
+  const objectivityScore = resultsObj.results[2].outcome.score;
+  const trustScore = 100 - resultsObj.results[3].outcome.score;
 
   return (
     <div>
 
 <Grid container>
 
+    <Grid item xs={12}>
       <ArticleSummary displayURL={analysedURL} displayArticle={article}/>
-      <BProgressBar/>  
-      <Grid item xs={12} md={4}>
-        <Credibility outcome={resultsObj.results[0].outcome} />
-      </Grid>
-    
-      <Grid item xs={12} md={4}>
-        <Objectivity displayObjectivity={objectivityScore}/>
-      </Grid>
+    </Grid>
+    <Grid item xs={12}>
+      <BProgressBar Bscore={trustScore}/> 
+    </Grid> 
+    <Grid item xs={12} md={4}>
+      <Credibility outcome={credibilityObj} />
+    </Grid>
+    <Grid item xs={12} md={4}>
+      <Objectivity displayObjectivity={objectivityScore}/>
+    </Grid>
 
 
 
