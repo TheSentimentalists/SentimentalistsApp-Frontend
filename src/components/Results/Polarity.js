bf4f532b9c50a34e33954a69aca7201638a1f2a0
Ellie Maidment <ellie947@hotmail.com>
2020-11-02 16:17:43 +0000

    format Polarity.js

--- src/components/Results/Polarity.js
+++ src/components/Results/Polarity.js
@@ -13,7 +13,6 @@ const useStyles = makeStyles({
     paddingBottom: "10px",
   },
 
-
   polarityText: {
     textAlign: "left",
     paddingLeft: "35px",
@@ -26,27 +25,27 @@ const useStyles = makeStyles({
 
   polarityHelp: {
     color: "#6bb26d",
-    cursor: "pointer"
+    cursor: "pointer",
   },
 });
 
 function Polarity(props) {
   const classes = useStyles();
-  console.log(props.displayPolarity)
-  const polscore = (props.displayPolarity+1)/2; //normalize -1 to +1 scale to 0-1, then convert to a percentage
-  let polemoji = ''
+  console.log(props.displayPolarity);
+  const polscore = (props.displayPolarity + 1) / 2; //normalize -1 to +1 scale to 0-1, then convert to a percentage
+  let polemoji = "";
 
   if (polscore < 0.25) {
-    polemoji = '😡' 
+    polemoji = "😡";
   } else if (polscore >= 0.25 && polscore < 0.45) {
-    polemoji = '🙁' 
+    polemoji = "🙁";
   } else if (polscore >= 0.45 && polscore < 0.55) {
-    polemoji = '😐' 
-} else if (polscore >= 0.55 && polscore < 0.75) {
-    polemoji = '😊' 
-} else if (polscore >= 0.75 && polscore < 1) {
-    polemoji = '😃' 
-}
+    polemoji = "😐";
+  } else if (polscore >= 0.55 && polscore < 0.75) {
+    polemoji = "😊";
+  } else if (polscore >= 0.75 && polscore < 1) {
+    polemoji = "😃";
+  }
 
   return (
     <div>
@@ -63,7 +62,6 @@ function Polarity(props) {
             formatTextValue={() => polemoji}
             arcsLength={[0.45, 0.1, 0.45]}
             needleColor="#d9d9d9"
-            
           />
         </Grid>
         <Grid item xs={12}>
@@ -83,7 +81,8 @@ function Polarity(props) {
                 className={classes.polarityTooltip}
                 title={
                   <p style={{ fontSize: "16px" }}>
-                    Measures the emotion behind the article, from negative to positive.
+                    Measures the emotion behind the article, from negative to
+                    positive.
                   </p>
                 }
               >
