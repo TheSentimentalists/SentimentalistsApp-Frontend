5e32a87241f8c7494786deffe36a1527f6fefb8e
Ellie Maidment <ellie947@hotmail.com>
2020-11-02 16:15:43 +0000

    format TrustScore

--- src/components/Results/TrustScore.js
+++ src/components/Results/TrustScore.js
@@ -8,12 +8,12 @@ import HelpIcon from "@material-ui/icons/Help";
 const useStyles = makeStyles({
   trustScore: {
     paddingLeft: "30px",
-    paddingRight: "30px"
+    paddingRight: "30px",
   },
   trustScoreHeader: {
     textAlign: "left",
     paddingBottom: "20px",
-    marginLeft: "0!important"
+    marginLeft: "0!important",
   },
   trustScoreTooltip: {
     fontSize: "30px",
@@ -22,7 +22,7 @@ const useStyles = makeStyles({
   trustScoreHelp: {
     textAlign: "right",
     color: "#6bb26d",
-    cursor: "pointer"
+    cursor: "pointer",
   },
 });
 
@@ -35,15 +35,13 @@ function TrustScore(props) {
 
   if (percentage < 30) {
     fgcolor = "linear-gradient(to right, #7d1225, #e60202 )";
-    trustmoji = '🙁';
-
+    trustmoji = "🙁";
   } else if (percentage >= 30 && percentage < 70) {
     fgcolor = "linear-gradient(to right, #ffcf53, #e79b37)";
-    trustmoji = '😐';
-
+    trustmoji = "😐";
   } else {
     fgcolor = "linear-gradient(to right, #5ba45b, #396f5c)";
-    trustmoji = '😃';
+    trustmoji = "😃";
   }
 
   return (
@@ -51,25 +49,30 @@ function TrustScore(props) {
       <Grid container justify="center" className={classes.trustScore}>
         <Grid item xs={12}>
           <Grid container justify="left">
-          <Typography variant="h5" className={classes.trustScoreHeader}>
-            Trust Indicator {trustmoji} 
-          </Typography>
-          <Grid>
-            <Tooltip
+            <Typography variant="h5" className={classes.trustScoreHeader}>
+              Trust Indicator {trustmoji}
+            </Typography>
+            <Grid>
+              <Tooltip
                 className={classes.trustScoreTooltip}
                 title={
                   <p style={{ fontSize: "16px" }}>
-                    Trust indicator breifly provides Trustworthiness of an article. It is calculated with equal weightage on Credibility, Objectivity and Polarity of the Article. A Green progress bar means article has Trustworthiness greater than 70%, a yellow bar means the article has Trustworthiness between 30% to 70%  and a red bar means that the article has Trustworthiness less than 30%.
+                    Trust indicator breifly provides Trustworthiness of an
+                    article. It is calculated with equal weightage on
+                    Credibility, Objectivity and Polarity of the Article. A
+                    Green progress bar means article has Trustworthiness greater
+                    than 70%, a yellow bar means the article has Trustworthiness
+                    between 30% to 70% and a red bar means that the article has
+                    Trustworthiness less than 30%.
                   </p>
                 }
               >
-              <HelpIcon className={classes.trustScoreHelp} />
-            </Tooltip>
+                <HelpIcon className={classes.trustScoreHelp} />
+              </Tooltip>
+            </Grid>
           </Grid>
-          </Grid>
-
         </Grid>
-        <Grid item xs={12}>  
+        <Grid item xs={12}>
           <ProgressBar
             percent={percentage}
             unfilledBackground="lightgrey"
